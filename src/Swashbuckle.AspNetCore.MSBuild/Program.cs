cb3f8b6f6b0c4e8eb4e7a90b2809be85f78f4285
Adam Fiksen <adfiksen@microsoft.com>
2018-04-15 23:20:44 -0700

    add MSBuild project

---
+++ src/Swashbuckle.AspNetCore.MSBuild/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Swashbuckle.AspNetCore.MSBuild
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/Swashbuckle.AspNetCore.MSBuild/SwaggerGenerator.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Build.Utilities;
+
+namespace Swashbuckle.AspNetCore.MSBuild
+{
+    class SwaggerGenerator : Task
+    {
+        public override bool Execute()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
