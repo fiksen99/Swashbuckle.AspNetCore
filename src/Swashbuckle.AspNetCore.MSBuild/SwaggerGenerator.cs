c3bdb9c5985692a5f5b8adf03b6b79f6e2afec1f
Adam Fiksen <adfiksen@microsoft.com>
2018-04-15 23:43:41 -0700

    add initial code to read and write the swagger

--- src/Swashbuckle.AspNetCore.MSBuild/SwaggerGenerator.cs
+++ src/Swashbuckle.AspNetCore.MSBuild/SwaggerGenerator.cs
@@ -1,5 +1,14 @@
-﻿using Microsoft.Build.Framework;
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
+using System;
+using System.IO;
+using System.Reflection;
 
 namespace Swashbuckle.AspNetCore.MSBuild
 {
@@ -17,7 +26,33 @@ namespace Swashbuckle.AspNetCore.MSBuild
 
         public override bool Execute()
         {
-            throw new System.NotImplementedException();
+            // 1) Configure host with provided startupassembly
+            var startupAssembly = Assembly.LoadFrom(Path.Combine(Directory.GetCurrentDirectory(), AssemblyToLoad));
+            var host = WebHost.CreateDefaultBuilder()
+                .UseStartup(startupAssembly.FullName)
+                .Build();
+
+            // 2) Retrieve Swagger via configured provider
+            var swaggerProvider = host.Services.GetRequiredService<ISwaggerProvider>();
+            var swagger = swaggerProvider.GetSwagger(
+                SwaggerDoc,
+                Host,
+                BasePath,
+                null);
+
+            // 3) Serialize to specified output location or stdout
+            var outputPath = Path.Combine(Directory.GetCurrentDirectory(), "swagger.json");
+
+            using (var streamWriter = (outputPath != null ? File.CreateText(outputPath) : Console.Out))
+            {
+                var mvcOptionsAccessor = (IOptions<MvcJsonOptions>)host.Services.GetService(typeof(IOptions<MvcJsonOptions>));
+                var serializer = SwaggerSerializerFactory.Create(mvcOptionsAccessor);
+
+                serializer.Serialize(streamWriter, swagger);
+                Console.WriteLine($"Swagger JSON succesfully written to {outputPath}");
+            }
+
+            return true;
         }
     }
 }
