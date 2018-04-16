46f6bf19570a00a6a35d21f29de92a92c5444dc3
Adam Fiksen <adfiksen@microsoft.com>
2018-04-15 23:21:42 -0700

    add assembly to load property

--- src/Swashbuckle.AspNetCore.MSBuild/SwaggerGenerator.cs
+++ src/Swashbuckle.AspNetCore.MSBuild/SwaggerGenerator.cs
@@ -4,6 +4,8 @@ namespace Swashbuckle.AspNetCore.MSBuild
 {
     class SwaggerGenerator : Task
     {
+        public string AssemblyToLoad { get; set; }
+
         public override bool Execute()
         {
             throw new System.NotImplementedException();
